a9461763cd1746c29a6f72757fa39e8f9b5fa8ff
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-06-09 12:10:57 -0300

    Criação da função formatar_data, transformação na tabela de compras, mesclando tabela em uma tabela final

--- Desafio/main.py
+++ Desafio/main.py
@@ -1,5 +1,3 @@
-from lib2to3.pgen2.pgen import DFAState
-from re import T
 import pandas as pd
 
 # Extração dos dados
@@ -12,21 +10,46 @@ df_shows = pd.read_json(URL_SHOWS)
 df_ingressos = pd.read_json(URL_INGRESSOS)
 df_compras = pd.read_csv(URL_COMPRAS)
 
+
+def formatar_data(df):
+    df = df.rename(columns={'ano': 'year', 'mes': 'month', 'dia': 'day'})
+    df['data'] = pd.to_datetime(df[['year', 'month', 'day']], format='%Y%m%d')
+    df = df.drop(['year', 'month', 'day'], axis=1)
+    return df
+
 # Transformações das tabelas
-tabela_shows = df_shows.T
-tabela_shows = tabela_shows.rename(columns={'ano': 'year', 'mes': 'month', 'dia': 'day'})
-tabela_shows['data'] = pd.to_datetime(tabela_shows, format='%Y%m%d')
-tabela_shows = tabela_shows.drop(['year', 'month', 'day'], axis=1)
+tabela_shows = formatar_data(df_shows.transpose())
 tabela_shows = tabela_shows.reset_index()
 tabela_shows = tabela_shows.rename(columns={'index': 'show'})
 
-print(tabela_shows)
-tabela_ingressos = df_ingressos
-tabela_ingressos = tabela_ingressos.rename(columns={'ano': 'year', 'mes': 'month', 'dia': 'day'})
-tabela_ingressos['data'] = pd.to_datetime(tabela_ingressos[['year', 'month', 'day']], format='%Y%m%d')
-tabela_ingressos = tabela_ingressos.drop(['year', 'month', 'day'], axis=1)
+tabela_ingressos = formatar_data(df_ingressos)
 tabela_ingressos = tabela_ingressos[['data', 'nome', 'status', 'tipo']]
+# tabela_ingressos['show'] = None
+tabela_compras = df_compras.groupby(['nome', 'show'])['gastos'].sum().reset_index()
+print(tabela_compras)
 
-print(tabela_ingressos.head())
+#print(df_compras.head())
+df_final = tabela_ingressos.merge(tabela_shows)
+df_final = df_final.merge(tabela_compras)
+print(df_final[(df_final['nome'] == 'John Rich') & (df_final['status'] == 'Concluido')])
+print(df_compras[df_compras['nome'] == 'John Rich'])
 
-# Média de gastos de pessoas com ingresso pista
+
+"""
+for indice in tabela_ingressos[tabela_ingressos['status'] == 'Concluido'].index:
+    data = tabela_ingressos['data'][indice]
+    indice_show = tabela_shows[tabela_shows['data'] == data].index[0]
+    show = tabela_shows.at[indice_show, 'show']
+    tabela_ingressos['show'][indice] = show
+"""
+
+# print(tabela_ingressos[(tabela_ingressos['status'] == 'Concluido') & (tabela_ingressos['tipo'] == 'Pista')])
+# TODO 1: Média de gastos de pessoas com ingresso pista
+
+# TODO 2: Pessoas não compareceram aos shows
+
+# TODO 3: Pessoas que compraram ingressos com os concorrentes
+
+# TODO 4: Dia com maior gasto
+
+# TODO 5: no pdf
